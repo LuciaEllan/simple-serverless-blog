ae4438d4563d6da13d8bbe556db0d6f7c6656618
Lucia Ellan <lucia.ellan@gmail.com>
2019-02-20 17:17:43 +0900

    fixed leftbar loop and some spinner

--- quasar/src/pages/TaggedPostList.vue
+++ quasar/src/pages/TaggedPostList.vue
@@ -7,10 +7,15 @@
       </q-card-section>
       <q-separator inset />
       <q-card-section>
-        <div v-for="post in postsData" :key="post.id" class="blog_post_body">
-          <router-link :to="`/post/${post.id}`">{{ post.title }}</router-link>
-          <q-icon v-if="!post.is_public" name="lock" color="accent" class="q-ml-xs" />
-          <span class="text-caption text-grey q-mx-md">{{ getPostDateDisplay(post) }}</span>
+        <template v-if="hasPost">
+          <div v-for="post in postsData" :key="post.id" class="blog_post_body">
+            <router-link :to="`/post/${post.id}`">{{ post.title }}</router-link>
+            <q-icon v-if="!post.is_public" name="lock" color="accent" class="q-ml-xs" />
+            <span class="text-caption text-grey q-mx-md">{{ getPostDateDisplay(post) }}</span>
+          </div>
+        </template>
+        <div v-else>
+          Ugh, we don't have any posts for this tag.
         </div>
       </q-card-section>
     </q-card>
@@ -40,6 +45,9 @@ export default {
   computed: {
     isLoadingPost () {
       return (this.postsData !== undefined) && (this.postsData.length === 0)
+    },
+    hasPost () {
+      return (this.postsData !== undefined) && (this.postsData.length > 0)
     }
   },
   methods: {
@@ -56,6 +64,9 @@ export default {
       query.get().then(result => {
         this.postsData = this.postsData.concat(result.docs.map((doc, index, docs) => ({ id: doc.id, ...doc.data() })))
         if (result.empty) {
+          if (this.postsData.length === 0) {
+            this.postsData = undefined
+          }
           this.hasMorePosts = false
         } else {
           this.lastPostRef = result.docs[result.docs.length - 1]
@@ -64,7 +75,12 @@ export default {
       }).catch(error => {
         this.postsData = undefined
         this.hasMorePosts = false
-        console.log('error during getting post digest: ' + error)
+        if (!error) console.log(error) // just makes linter shut up :X
+        this.$q.notify({
+          message: `Sorry, cannot find any posts. Did you signed in for sure, right?`,
+          position: 'top-right',
+          color: 'negative'
+        })
       })
     },
     getPostDateDisplay (post) {
