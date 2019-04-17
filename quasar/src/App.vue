1a3f78103481ea39949535832d08991ad41b5523
Lucia Ellan <lucia.ellan@gmail.com>
2019-04-17 20:43:12 +0900

    moved firebase init to boot

--- quasar/src/App.vue
+++ quasar/src/App.vue
@@ -6,15 +6,10 @@
 </template>
 
 <script>
-import firebase from 'firebase/app'
-import FirebaseConfig from './configs/firebase-config'
 import FirebaseAuth from 'src/components/FirebaseAuth'
 
 export default {
   name: 'App',
-  created () {
-    firebase.initializeApp(FirebaseConfig)
-  },
   components: {
     FirebaseAuth
   }
